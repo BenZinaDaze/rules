3488cc56c491dc0803c529921f92523ed7edc523
benz1 <o3325qq@gmail.com>
2022-12-04 20:57:50 +0800

    test alook open

--- Quantumult-X/MdUnlock.js
+++ Quantumult-X/MdUnlock.js
@@ -2,6 +2,7 @@ let url = $request.url; // 视频连接
 let headers = $request.headers;
 if (headers.hasOwnProperty("authorization")) {
     $notify(">_ 视频链接已经捕获成功 🛠", ">_ 点击一下复制视频链接 🔞", "", {
+        "open-url": url.replace("https","alook"),
         "update-pasteboard": url
     });
     $done({path: "/null", headers : headers});
