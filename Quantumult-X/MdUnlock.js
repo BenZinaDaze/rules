37a52dd0ec9b76f0b3b3df63cff1cd02869d3319
benz1 <o3325qq@gmail.com>
2022-12-04 18:28:20 +0800

    init

---
+++ Quantumult-X/MdUnlock.js
@@ -0,0 +1,10 @@
+let url = $request.url; // 视频连接
+let headers = $request.headers;
+if (headers.hasOwnProperty("authorization")) {
+    $notify(">_ 视频链接已经捕获成功 🛠", ">_ 点击一下复制视频链接 🔞", "", {
+        "update-pasteboard": url
+    });
+    $done({path: "/null", headers : headers});
+} else { 
+    $done();
+}
